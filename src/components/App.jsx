b6afe8c4eb7e011eb6cc4da6c89cabb721b54de5
Bordiuzhan <bordyujan@gmail.com>
2022-12-07 23:05:16 +0100

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import { nanoid } from 'nanoid';
+import { Layout } from './Layout';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = data => {
+    const { name, number } = data;
+    for (let contact of this.state.contacts) {
+      if (contact.name.toLowerCase().includes(name.toLowerCase())) {
+        return alert(`${name} is alredy in contacts`);
+      }
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredName = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredName = this.getFilteredName();
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact}></ContactForm>
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter}></Filter>
+        <ContactList
+          items={filteredName}
+          onDeleteContact={this.deleteContact}
+        ></ContactList>
+      </Layout>
+    );
+  }
+}
